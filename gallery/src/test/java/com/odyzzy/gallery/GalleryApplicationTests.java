b9ec9369487f9c760aa9d94de89202102b759674
Pasindu Chandrasekara <pasinduchandrasekara@Pasindus-Mac-mini.local>
2023-11-30 21:25:02 +0800

    first commit

---
+++ gallery/src/test/java/com/odyzzy/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odyzzy.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
